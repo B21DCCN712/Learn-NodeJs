eaf7552a8f2c12bce7d6534ac91e69098bb86568
B21DCCN712 <toannobi2k3@gmail.com>
2025-10-04 23:11:21 +0700

    connection DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,19 +1,28 @@
-
+const connection = require("../config/database");
 const getHomepage = (req, res) => {
-    res.send('Hello World! And Nodemon')
-}
+    let users = [];
+
+    // A simple SELECT query
+    connection.query("SELECT * FROM Users u", function (err, results, fields) {
+        users = results;
+        console.log(">>>results home page = ", results); // results contains rows returned by server
+
+        console.log(">>check users: ", users);
+        res.send(JSON.stringify(users));
+    });
+};
 
 const getABC = (req, res) => {
-    res.send('Check ABC')
-}
+    res.send("Check ABC");
+};
 
 const getPresent111 = (req, res) => {
     //res.send('<h1>Present111</h1>')
-    res.render('sample.ejs')
-}
+    res.render("sample.ejs");
+};
 
 module.exports = {
     getHomepage,
     getABC,
-    getPresent111
-}
+    getPresent111,
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const path = require('path')
-require('dotenv').config();
-const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web');
+const express = require("express");
+const path = require("path");
+require("dotenv").config(); //ket noi file .env
+require("dotenv").config();
+
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 //console.log('>>> check env', process.env);
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
@@ -13,8 +15,13 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 
 //khai bao route
-app.use('/', webRoutes);
+app.use("/", webRoutes);
+
+// A simple SELECT query
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+    console.log(">>>results= ", results); // results contains rows returned by server
+});
 
 app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
